ae85b09b6d70fafa7a85255a5ac691e65a70222d
huxaiphaer <huxaiphaeridris@gmail.com>
2020-04-30 16:55:09 +0300

    (feat) Implement UI Tests and CI/CD: - Setting up Jobs for CI/CD - Setting up UI Tests

--- web-app/jest.config.js
+++ web-app/jest.config.js
@@ -1,34 +1,5 @@
-process.env.VUE_CLI_BABEL_TARGET_NODE = true;
-process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
-
 module.exports = {
-    moduleFileExtensions: [
-        'js',
-        'jsx',
-        'json',
-        'vue'
-    ],
-    transform: {
-        '^.+\\.vue$': 'vue-jest',
-        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-        '^.+\\.jsx?$': 'babel-jest'
-    },
-    transformIgnorePatterns: [
-        "node_modules/(?!(babel-jest|jest-vue-preprocessor)/)"
-    ],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
-    },
-    snapshotSerializers: [
-        'jest-serializer-vue'
-    ],
-    testMatch: [
-        '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-    ],
-    testURL: 'http://localhost/',
+    preset:"@vue/cli-plugin-unit-jest",
     collectCoverage: true,
-    collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**',
-        '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**'
-    ],
-    coverageReporters: ['lcov', 'text-summary']
+    collectCoverageFrom:["src/**/*.{js,vue}"]
 }
